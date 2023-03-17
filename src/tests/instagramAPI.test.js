728443c79c174e85bcd80a0e2701696c2a87dbbe
lealalves <leealalves@hotmail.com>
2023-03-17 14:45:36 -0300

    added tests for service instagramApi

--- src/tests/instagramAPI.test.js
+++ src/tests/instagramAPI.test.js
@@ -1,9 +1,37 @@
+import { deepEqual, notDeepStrictEqual,ok } from 'node:assert'
+
 import * as dotenv from 'dotenv'
-dotenv.config({path: '../../.env'})
+dotenv.config()
 
 import InstagramApi from '../services/instagramAPI.js'
 
 const token = process.env.TOKEN_INSTAGRAM
 const api = new InstagramApi(token)
 
-console.log(await api.getPosts())
+const MOCK_USERNAME_PROFILE = {
+  username: 'leallealleal1'
+}
+
+describe('Suite de testes do serviço InstagramAPI', function() {
+  it('testApi() deve retornar TRUE', async () => {
+    const result = await api.testApi()
+    ok(result)
+  })
+
+  it('getPosts() deve retornar um array', async () => {
+    const result = await api.getPosts()
+    ok(Array.isArray(result))
+  })
+
+  it('getPosts() deve retornar o nome de usuário', async () => {
+    const [{ username }] = await api.getPosts()
+    deepEqual(username, MOCK_USERNAME_PROFILE.username)
+  })
+
+  it('getPosts() deve retornar URL e Legenda da publicação', async () => {
+    const [{ caption, media_url }] = await api.getPosts()
+
+    notDeepStrictEqual(caption, undefined)
+    notDeepStrictEqual(media_url, undefined)
+  })
+})
