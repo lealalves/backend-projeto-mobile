686cadf7db20cb3caa531e3e03f88052a90f5dce
lealalves <leealalves@hotmail.com>
2023-03-27 13:18:54 -0300

    tests for services routes

---
+++ src/tests/serviceRoutes.test.js
@@ -0,0 +1,22 @@
+import { deepEqual, notDeepStrictEqual,ok } from 'node:assert'
+import api from '../api.js'
+
+let app = {}
+
+describe.only('Suite de testes serviços InstagramApi', function () {
+  this.beforeAll(async () => {
+    app = await api
+  })
+
+  it('GET /service/instagram_posts - Deve retornar um array de posts', async () => {
+    const result = await app.inject({
+      method: 'GET',
+      url: '/services/instagram_posts'
+    })
+
+    const dados = JSON.parse(result.payload)
+
+    ok(result.statusCode === 200)
+    ok(Array.isArray(dados))
+  })
+})
