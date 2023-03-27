47eb23061df4132ab74c4407e1ffe92bfdc6453b
lealalves <leealalves@hotmail.com>
2023-03-27 13:18:21 -0300

    added config for cors and routes for services

--- src/api.js
+++ src/api.js
@@ -1,15 +1,28 @@
-import Hapi from '@hapi/hapi'
-import MongoDB from './db/mongodb/mongodb.js'
-import userSchema from './db/mongodb/schemas/userSchema.js'
-import userRoutes from './routes/userRoutes.js'
+import * as dotenv from 'dotenv'
+dotenv.config({path: '../.env'})
 
+import Hapi from '@hapi/hapi'
 import HapiSwagger from 'hapi-swagger'
 import Vision from '@hapi/vision'
 import Inert from '@hapi/inert'
 
+import MongoDB from './db/mongodb/mongodb.js'
+import userSchema from './db/mongodb/schemas/userSchema.js'
+import userRoutes from './routes/userRoutes.js'
+
+import instagramAPI from './services/instagramAPI.js'
+import serviceRoutes from './routes/serviceRoutes.js'
+
+const INSTA_TOKEN = process.env.TOKEN_INSTAGRAM
 
 const app = new Hapi.server({
-  port: 5000
+  port: 5000,
+  routes: {
+    cors: {
+      origin: ['*'],
+      credentials: true
+    }
+  }
 })
 
 function mapRoutes(instance, methods){
@@ -19,6 +32,7 @@ function mapRoutes(instance, methods){
 async function main() {
   const connection = MongoDB.connect()
   const context = new MongoDB(connection, userSchema)
+  const api = new instagramAPI(INSTA_TOKEN)
 
   const swaggerOptions = {
     info: {
@@ -35,8 +49,10 @@ async function main() {
     }    
   ])
 
-  app.route(
-    mapRoutes(new userRoutes(context), userRoutes.methods())
+  app.route([
+    ...mapRoutes(new userRoutes(context), userRoutes.methods()),
+    ...mapRoutes(new serviceRoutes(api), serviceRoutes.methods())
+    ]
   )
 
   app.start()
