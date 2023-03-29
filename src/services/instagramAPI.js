9652dce55bb36a3002ba65960d9333271b200d7a
lealalves <leealalves@hotmail.com>
2023-03-29 00:08:16 -0300

    code refactored
    Removido toda responsabilidade de validação do construtor do serviço.

--- src/services/instagramAPI.js
+++ src/services/instagramAPI.js
@@ -1,35 +1,21 @@
+import Boom from '@hapi/boom'
+
 export default class InstagramApi {
   constructor(token) {
     this._token = token
   }
 
   async testApi() {
-    try {
       const url = `https://graph.instagram.com/me/media?access_token=${this._token}`
       const statusCode = (await fetch(url)).status
-
-      if(statusCode !== 200) return false
-
-      return true
-
-    } catch (error) {
-      console.log('DEU RUIM', error);
-      return 'Erro interno na API'
-    }
+      
+      return statusCode === 200? true : false
   }
 
-  async getPosts(skip = 0, limit = 10){
-    try {
-      const fields = 'caption,id,media_type,media_url,username,timestamp'
-      const url = `https://graph.instagram.com/me/media?access_token=${this._token}&fields=${fields}`
-  
-      const data = (await (await fetch(url)).json()).data  
+  async getPosts(){
+    const fields = 'caption,id,media_type,media_url,username,timestamp'
+    const url = `https://graph.instagram.com/me/media?access_token=${this._token}&fields=${fields}`
 
-      return data.slice(skip, limit)
-      
-    } catch (error) {
-      console.log('DEU RUIM', error);
-      return 'Erro interno na API'
-    }
+    return await fetch(url)
   }
 }
