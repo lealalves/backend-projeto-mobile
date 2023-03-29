d7d0ca11ef75ce72cf117542e0a63aa366534cbc
lealalves <leealalves@hotmail.com>
2023-03-29 00:09:55 -0300

    code refactored
    Agora toda responsabilidade de validação está no codigo da rota

--- src/routes/serviceRoutes.js
+++ src/routes/serviceRoutes.js
@@ -16,15 +16,32 @@ export default class serviceRoutes extends baseRoute {
       path: '/services/instagram_posts',
       method: 'GET',
       config: {
+        auth: false,
         description: 'Deve retonar uma lista de posts do instagram.',
         notes: 'retorna um array com as postagens do perfil alvo',
         tags: ['api'],
+        validate: {
+          failAction,
+          query: Joi.object({
+            limit: Joi.number().integer().default(10),
+            skip: Joi.number().integer().default(0),
+          })
+        }
       },
       handler: async (request, headers) => {
         try {
+          const { 
+            skip, 
+            limit 
+          } = request.query
+          
           const result = await this.api.getPosts()
 
-          return result
+          if(result.status !== 200) return Boom.internal()
+
+          const dados = (await result.json()).data
+
+          return dados.slice(skip, limit)
         } catch (error) {
           console.log('DEU RUIM', error);
           Boom.internal()
