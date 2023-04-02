9ca688b43b8a7ddd6ee3b1f52febd8e1c76ab87f
lealalves <leealalves@hotmail.com>
2023-04-02 20:11:41 -0300

    updated failAction

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,7 +6,8 @@ import passwordHelper from '../helpers/passwordHelper.js'
 
 
 const failAction = (request, headers, error) => {
-  throw error
+  let errors = error.output.payload.validation.keys[0]
+  throw Boom.badRequest(`o campo ${errors} não é permitido estar vazio.`)
 }
 
 export default class authRoutes extends baseRoute {
@@ -29,14 +30,14 @@ export default class authRoutes extends baseRoute {
           failAction,
           payload: Joi.object({
             email: Joi.string().required(),
-            password: Joi.string().required()
+            senha: Joi.string().required()
           })
         }
       }, handler: async (request, headers) => {
         try {
           const { 
             email, 
-            password 
+            senha 
           } = request.payload
   
           const [usuario] = await this.db.listar({
@@ -45,7 +46,7 @@ export default class authRoutes extends baseRoute {
   
           if(!usuario) return Boom.unauthorized('Usuário não encontrado!')
 
-          const match = await passwordHelper.comparePassword(password, usuario.senha)
+          const match = await passwordHelper.comparePassword(senha, usuario.senha)
   
           if(!match) return Boom.unauthorized('Usuário ou senha inválida')
   
